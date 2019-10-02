bc41b8bd05cf504046fe8b89736e9049660eaa39
ThiliniWathsala <o.t.wathsala@gmail.com>
2019-10-02 22:39:28 +0100

    fix udate image

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -60,12 +60,19 @@ router.post("",multer({storage:storage}).single("image"),(req,res)=>{    //singl
   
   
   
-  router.put("/:id",(req,res,next)=>{
+  router.put("/:id",multer({storage:storage}).single("image"),(req,res,next)=>{
+    let imagePath =req.body.imagePath;
+    if(req.file){
+      const url = req.protocol + '://' + req.get("host"); 
+      imagePath = url + "/images/" + req.file.filename
+    }
     const post =new Post({
       _id:req.body._id,
       title:req.body.title,
-      content:req.body.content
+      content:req.body.content,
+      imagePath:imagePath
     });
+    console.log(post)
     
     Post.updateOne({_id:req.params.id},post).then(result=>{   // id id get from url
       console.log(result);
